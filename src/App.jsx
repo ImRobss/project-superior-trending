f7adf0f00a4b65feb43a4395c24f6dd1426df2ca
ImRobss <robson.queirozwe@gmail.com>
2026-02-19 13:32:07 -0300

    added new imports and smooth scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
 import "./App.css";
+import { useEffect } from "react";
+import Lenis from "@studio-freight/lenis";
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Itens from "./components/Itens";
+import Orders from "./components/Orders";
 
 function App() {
+  useEffect(() => {
+    const lenis = new Lenis({
+      duration: 1.2,
+      smoothWheel: true,
+      smoothTouch: false,
+    });
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
   return (
     <>
       <Hero />
       <Itens />
+      <Orders />
+      <Footer />
     </>
   );
 }
