17b249e124eec49bf96705019cdfebaa3250659b
ImRobss <robson.queirozwe@gmail.com>
2026-02-19 10:39:43 -0300

    feat: app project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Hero from "./components/Hero";
+import Itens from "./components/Itens";
+
+function App() {
+  return (
+    <>
+      <Hero />
+      <Itens />
+    </>
+  );
+}
+
+export default App;
