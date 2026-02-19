2821a14566157d4b8a6a9d5a6ca7dc38ff70cc0a
ImRobss <robson.queirozwe@gmail.com>
2026-02-19 10:38:43 -0300

    feat: add Card (UI) component

---
+++ src/components/ui/Card.jsx
@@ -0,0 +1,15 @@
+export default function Card({ title, description }) {
+  return (
+    <div className="h-auto w-[500px] bg-white p-4 rounded-lg shadow-lg">
+      <img
+        src="/images/logo.jpg"
+        alt={title}
+        className="w-full h-auto object-cover mb-4"
+      />
+      <div className="text-center px-5">
+        <h2 className=" mb-2">{title}</h2>
+        <p className="text-gray-600">{description}</p>
+      </div>
+    </div>
+  );
+}
